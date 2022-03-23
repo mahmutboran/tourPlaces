5d81d209975a128ab649651147c7b25b185ed72d
mahmutboran <86852026+mahmutboran@users.noreply.github.com>
2022-03-23 23:34:03 +0300

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.scss"
+const Header = ()=>{
+    return(
+        <div className="header">
+            <h1>POPULER TOUR PLACES</h1>
+        </div>
+    )
+}
+export default Header;
